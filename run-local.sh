33f98bccb475110f5f8cb8ce29f6e84c6fb092c1
Agustin Rojas <rojasagusin90@gmail.com>
2019-08-19 23:01:20 -0300

    Fix run local

--- run-local.sh
+++ run-local.sh
@@ -1,1 +1,1 @@
-docker-compose -f docker-compose.yml -f local-docker-compose.yml up --build --scale cte_data_owner=$1
+docker-compose -f docker-compose.yml -f local-docker-compose.yml up --build --remove-orphans --scale cte_data_owner=$1
